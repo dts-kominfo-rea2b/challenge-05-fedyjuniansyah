bdd493096da486ff7c294c1f319430350133e42f
Fedy Juniansyah <fedyjuniansyah@gmail.com>
2022-06-04 20:38:31 +0700

    Challenge Callback Done

--- index.js
+++ index.js
@@ -6,19 +6,41 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, x)=>{ 
+   const list = [];
+   let result = x(names);
+  
+   for( let a = 0; a < result.length; a++){
+     b = a + 1 + ". "+ result[a];
+     list.push(b);
+   }
+ 
+  return list;
+
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) =>{   
+  let c = names.sort((a, b) => (a > b ? 1 : -1));
+ // console.log(c);
+  return c;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) =>{
+  let d = names.sort((a, b) => (a > b ? -1 : 1));
+ // console.log(names);
+  return d;
+};
+
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
+  console.log('Contoh kembalian dari fungsi sortAscending')
   console.log(sorter?.(names, sortAscending)?.join("\n"));
+  console.log('\n Contoh kembalian dari fungsi sortDescending')
   console.log(sorter?.(names, sortDescending)?.join("\n"));
 })();
 
